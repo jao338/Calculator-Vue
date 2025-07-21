5b2b63c8d12d952ba0d614a68f962254c5539047
João Henrique dos Santos Pereira <santos.joao@crmsolucoes.com.br>
2025-07-21 09:48:43 -0300

    fix: Configurando/Corrigindo erros e avisos do typescript

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import {router} from './router/router.ts'
+import {router} from './router/router'
 
 const app = createApp(App)
 
--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -1,7 +1,10 @@
 <template>
   <div>A</div>
+  <div>{{ nome }}</div>
 </template>
 
 <script setup lang="ts">
+
+const nome: number = 'isso deve quebrar'
 
 </script>
